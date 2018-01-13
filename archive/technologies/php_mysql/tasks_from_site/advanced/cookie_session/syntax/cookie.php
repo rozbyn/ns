e499e5ce4112948670458c54bf2467f0ebccb91c
rozbyn <rozbyn@yandex.ru>
2018-01-13 19:00:04 +0300

    new lesson cookie

---
+++ archive/technologies/php_mysql/tasks_from_site/advanced/cookie_session/syntax/cookie.php
@@ -0,0 +1,39 @@
+<?php
+date_default_timezone_set('Europe/Moscow');
+
+//Запишем в куки с именем test значение 'Тест!':
+setcookie('test', 'Тест!');
+
+//Запишем куку на час (в часе 3600 секунд!):
+setcookie("test","Тест!", time() + 3600); 
+
+//Запишем куку на день (в сутках 3600*24 секунд!):
+setcookie("test","Тест!", time() + 3600*24); 
+
+//Запишем куку на месяц (в месяце 3600*24*30 секунд!):
+setcookie("test","Тест!", time() + 3600*24*30); 
+
+//Запишем куку на год (в году 3600*24*30*365 секунд!):
+setcookie("test","test3", time() + 3600*24*365); 
+
+echo $_COOKIE['test'];
+
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Cookie & Sessions</title>
+   
+</head>
+<body >
+	<div class="main">
+		<div class="wrapper">
+
+		</div>
+	</div>
+	<div style="position: fixed; top:90%; left:80%; z-index:9; background: #f9f98c;"><a href="http://theory.phphtml.net/books/php/auth/rabota-s-cookie-na-php.html" target="_blank">Страница учебника</a></div>
+</body>
+</html>
