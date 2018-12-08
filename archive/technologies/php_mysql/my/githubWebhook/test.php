cb0bb07fea2e1c1d13a0a009d3f7e26c5cd72e13
rozbyn <rozbyn@yandex.ru>
2018-12-08 21:38:54 +0300

    Update test.php

--- archive/technologies/php_mysql/my/githubWebhook/test.php
+++ archive/technologies/php_mysql/my/githubWebhook/test.php
@@ -1,5 +1,9 @@
 <?php
 
+
+'https://raw.githubusercontent.com/rozbyn/ns/master/archive/technologies/php_mysql/copied_examples/Design%20Patterns/adapter.php';
+
+
 if(isset($_REQUEST['payload'])){
 	$payload = json_decode($_REQUEST['payload'], true);
 	file_put_contents('jopz.txt', var_export($payload, true));
