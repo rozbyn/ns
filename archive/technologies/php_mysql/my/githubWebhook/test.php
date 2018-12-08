7096ce7b6329c95e020a49fda5b4630445c7841d
rozbyn <rozbyn@yandex.ru>
2018-12-08 20:02:02 +0300

    Update test.php

--- archive/technologies/php_mysql/my/githubWebhook/test.php
+++ archive/technologies/php_mysql/my/githubWebhook/test.php
@@ -2,6 +2,6 @@
 
 if(isset($_REQUEST['payload'])){
 	$payload = json_decode($_REQUEST['payload'], true);
-	file_put_contents('jopz.txt', var_export($_REQUEST, true));
+	file_put_contents('jopz.txt', var_export($payload, true));
 	//dsada
 }
