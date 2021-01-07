b5a496b1331db7dd5b6b04240c32ee624071ea73
Rozbyn4 <rozbyn@yandex.ru>
2021-01-07 11:50:08 +0300

    в

--- archive/examples/browserExt/chrome/downloadTrackVK/pageScript.js
+++ archive/examples/browserExt/chrome/downloadTrackVK/pageScript.js
@@ -1,7 +1,8 @@
 
-/* global saveVkAudioExtensionKey, SourceBuffer, chrome, contentScriptTabs, vk, saveVkAudioThisTabID, AdsLight, ap, URL */
+/* global saveVkAudioExtensionKey, SourceBuffer, chrome, contentScriptTabs, vk, saveVkAudioThisTabID, AdsLight, ap, URL, audio, playerAdapter */
 
 ;javascript:(function(){
+	
 	setInterval(function () {
 		try {AdsLight.setNewBlock = function () {};} catch (e) {}
 		try {ap._checkAdsPlay = function (a,b,cb) {if(typeof cb === 'function') {cb();}};} catch (e) {}
@@ -13,7 +14,12 @@
 		}
 
 	}, 1000);
-
+	
+	var CAN_WORK = false;
+	var IS_MOBILE_PLAYER = false;
+	
+	
+	
 	if(!saveVkAudioExtensionKey || !saveVkAudioThisTabID) return;
 //	document.head.removeChild(document.getElementById('saveVkAudioScript2'));
 	sendMessageToBackgroundScript('pageScriptReady', tracksInfo);
@@ -37,16 +43,28 @@
 	
 	var origSBAppend = SourceBuffer.prototype.appendBuffer;
 	SourceBuffer.prototype.appendBuffer = function (source) {
+//		console.log(this, this.ended, source);
 		saveSB(this, source);
 		return origSBAppend.apply(this, arguments);
 	};
 
 
 	function saveSB (sB, arBin) {
-		if(!window.ap._isPlaying) return;
+		if(
+				!checkAudioPlayer()
+				|| (!IS_MOBILE_PLAYER && !window.ap._isPlaying)
+				|| (IS_MOBILE_PLAYER && !window.audio.isPlaying())
+		){
+			return;
+		}
 		var i = sourceBufStor.indexOf(sB);
 		if(i === -1){
 			var trackInfo = getCurrentTrackInfo();
+			if(trackInfo.id in tracksInfo){
+				trackInfo = getNextTrackInfo();
+				if(!trackInfo) return;
+//				console.log('DANGER!');
+			}
 			sB.trackID = trackInfo.id;
 			trackInfo['soundBuffer'] = sB;
 			trackInfo['status'] = 'loading';
@@ -100,21 +118,21 @@
 		var a = document.createElement('a');
 		a.style.display = 'none';
 		document.body.appendChild(a);
-		a.download = file.name;
+		a.download = htmlDecode(file.name);
 		a.href = d;
 		a.click();
 		document.body.removeChild(a);
 	}
-
-	function getCurrentAudioFilename() {
-		if(window.ap && window.ap.getCurrentAudio){
-			var a = window.ap.getCurrentAudio();
-			if(a[3] && a[4]){
-				return a[4] + ' - ' + a[3] + '.mp3';
-			}
-		}
-		return 'track.mp3';
+	
+	
+	
+	
+	function htmlDecode(text) {
+		var e = document.createElement('textarea');
+		e.innerHTML = text;
+		return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
 	}
+
 	
 	
 	
@@ -133,9 +151,41 @@
 	
 	function getCurrentTrackInfo() {
 		var trackInfo = {};
-		var a = window.ap.getCurrentAudio();
-		trackInfo['id'] = a[1] + '_' + a[0];
-		trackInfo['name'] = a[4] + ' - ' + a[3] + '.mp3';
+		if(!('ap' in window) && ('audio' in window) && ('audioplayer' in window)){
+			var a = window.audio.getCurrent();
+			trackInfo['id'] = a.fullId; // 5675103_456239174
+			trackInfo['name'] = a.performer + ' - ' + a.title + '.mp3';
+		} else if ('ap' in window) {
+			a = window.ap.getCurrentAudio();
+			trackInfo['id'] = a[1] + '_' + a[0]; // 5675103_456239140
+			trackInfo['name'] = a[4] + ' - ' + a[3] + '.mp3'; // Hatom - Hard Street (Исполнитель - Трек)
+		}
+		trackInfo['tabID'] = saveVkAudioThisTabID;
+		return trackInfo;
+	}
+	
+	
+	function getNextTrackInfo() {
+		var trackInfo = {};
+		if(!('ap' in window) && ('audio' in window) && ('audioplayer' in window)){
+			var a = window.audio.getCurrent();
+			var playlistArray = window.audio.playlist();
+			var nextAudio = false;
+			for (var i = 0; i < playlistArray.length; i++) {
+				if(playlistArray[i].fullId === a.fullId && playlistArray[i+1]){
+					nextAudio = playlistArray[i+1];
+					break;
+				}
+			}
+			if(!nextAudio) return false;
+			trackInfo.id = nextAudio.fullId; // 5675103_456239174
+			trackInfo.name = nextAudio.performer + ' - ' + nextAudio.title + '.mp3';
+			
+		} else if ('ap' in window) {
+			a = window.ap.getCurrentPlaylist().getNextAudio(window.ap.getCurrentAudio());
+			trackInfo.id = a[1] + '_' + a[0];
+			trackInfo.name = a[4] + ' - ' + a[3] + '.mp3';
+		}
 		trackInfo['tabID'] = saveVkAudioThisTabID;
 		return trackInfo;
 	}
@@ -160,7 +210,6 @@
 	window.saveVkAudio.sourceArrStor = sourceArrStor;
 	window.saveVkAudio.tracksInfo = tracksInfo;
 	window.saveVkAudio.audioStor = audioStor;
-	window.saveVkAudio.getCurrentAudioFilename = getCurrentAudioFilename;
 	window.saveVkAudio.saveFile = saveFile;
 	window.saveVkAudio.saveFileByDirectUrl = saveFileByDirectUrl;
 	window.saveVkAudio.sendMessage = sendMessage;
@@ -235,20 +284,71 @@
 		chrome.runtime.sendMessage(saveVkAudioExtensionKey, objToSend, cb);
 	}
 	
+	
+	
+	function checkAudioPlayer() {
+		if('ap' in window){
+			IS_MOBILE_PLAYER = false;
+			CAN_WORK = true;
+		} else if (('audio' in window) && ('audioplayer' in window)) {
+			IS_MOBILE_PLAYER = true;
+			CAN_WORK = true;
+		} else {
+			CAN_WORK = false;
+		}
+		return CAN_WORK;
+	}
+	
 	window.saveVkAudio.sendMessageToPopupScript = sendMessageToPopupScript;
 	window.saveVkAudio.sendMessageToBackgroundScript = sendMessageToBackgroundScript;
 	window.saveVkAudio.sendMessageToContentScript = sendMessageToContentScript;
 	
+	checkAudioPlayer();
 	
 	
-	var directLinkRegex = /^https:\/\/.+?audios\/\w+\.(mp3|wav|wave|wma|ogg|aac|ac3)/;
-	window.ap.eventBus.subscribe('start', async function () {
-		var audioSrc = window.ap._impl._currentAudioEl.src;
-		var isDirectLink = directLinkRegex.test(audioSrc);
-		if(isDirectLink && !window.ap._impl._currentHls){ // Трек доступен по прямой ссылке
+	// playerAdapter
+//	console.log(checkAudioPlayer() && !IS_MOBILE_PLAYER);
+	if(checkAudioPlayer() && !IS_MOBILE_PLAYER){
+		window.ap.eventBus.subscribe('start', async function () {
+//			console.log(arguments, this);
+			var directLinkRegex = /^https:\/\/.+?\.(mp3|wav|wave|wma|ogg|aac|ac3)/;
+			var audioSrc = window.ap._impl._currentAudioEl.src;
+			var isDirectLink = directLinkRegex.test(audioSrc);
+			if(isDirectLink && !window.ap._impl._currentHls){ // Трек доступен по прямой ссылке
+				var trackInfo = getCurrentTrackInfo();
+				if(trackInfo.id in tracksInfo) return;
+				var response = await fetch(audioSrc);
+				var contentType = response.headers.get('content-type');
+				if(response.status !== 200 || contentType.indexOf('audio') !== 0){
+					return;
+				}
+				trackInfo.arrayBuffer = await response.arrayBuffer();
+				trackInfo.binArIndex = sourceArrStor.length;
+				sourceArrStor.push(trackInfo.arrayBuffer);
+				trackInfo.status = 'ready';
+	//			trackInfo.directLink = audioSrc;
+				tracksInfo[trackInfo.id] = trackInfo;
+
+
+
+				sendTracksInfo();
+			}
+		});
+	} else if (checkAudioPlayer() && IS_MOBILE_PLAYER && ('playerAdapter' in window)){
+		playerAdapter.listenPlay(async function (trackID) {
+			var directLinkRegex = /^https:\/\/.+?\.(mp3|wav|wave|wma|ogg|aac|ac3)/;
+			var vkTrackInfo = playerAdapter.getAudio();
+			var trackUrl = vkTrackInfo.url;
+			var decodedUrl = decodeTrackUrl(trackUrl);
+			if(
+					!directLinkRegex.test(decodedUrl) 
+					|| decodedUrl.indexOf('https://m.vk.com/mp3/audio_api_unavailable.mp3') === 0
+					|| (trackID in tracksInfo)
+			){
+				return;
+			}
 			var trackInfo = getCurrentTrackInfo();
-			if(trackInfo.id in tracksInfo) return;
-			var response = await fetch(audioSrc);
+			var response = await fetch(decodedUrl);
 			var contentType = response.headers.get('content-type');
 			if(response.status !== 200 || contentType.indexOf('audio') !== 0){
 				return;
@@ -257,14 +357,11 @@
 			trackInfo.binArIndex = sourceArrStor.length;
 			sourceArrStor.push(trackInfo.arrayBuffer);
 			trackInfo.status = 'ready';
-//			trackInfo.directLink = audioSrc;
 			tracksInfo[trackInfo.id] = trackInfo;
-			
-			
-			
 			sendTracksInfo();
-		}
-	});
+		});
+	}
+	
 
 
 /*
@@ -300,5 +397,78 @@ UPDATE: "update"
 VOLUME: "volume"
 
 */
+	
+	function decodeTrackUrl(e) {
+		var On = {
+				v: function(e) {
+						return e.split("").reverse().join("");
+				},
+				r: function(e, t) {
+						var n;
+						e = e.split("");
+						for (var i = kn + kn, a = e.length; a--; )
+								~(n = i.indexOf(e[a])) && (e[a] = i.substr(n - t, 1));
+						return e.join("");
+				},
+				s: function(e, t) {
+						var n = e.length;
+						if (n) {
+								var i = function(e, t) {
+										var n = e.length
+											, i = [];
+										if (n) {
+												var a = n;
+												for (t = Math.abs(t); a--; )
+														t = (n * (a + 1) ^ t + a) % n,
+														i[a] = t;
+										}
+										return i;
+								}(e, t)
+									, a = 0;
+								for (e = e.split(""); ++a < n; )
+										e[a] = e.splice(i[n - 1 - a], 1, e[a])[0];
+								e = e.join("");
+						}
+						return e;
+				},
+				i: function(e, t) {
+						return On.s(e, t ^ vk.id);
+				},
+				x: function(e, t) {
+						var n = [];
+						return t = t.charCodeAt(0),
+						each(e.split(""), (function(e, i) {
+								n.push(String.fromCharCode(i.charCodeAt(0) ^ t));
+						}
+						)),
+						n.join("");
+				}
+		};
+		
+		var kn = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN0PQRSTUVWXYZO123456789+/=';
+		function jn(e) {
+			if (!e || e.length % 4 == 1) return !1;
+			for (var t, n, i = 0, a = 0, o = ""; (n = e.charAt(a++)); )
+				~(n = kn.indexOf(n)) && (t = i % 4 ? 64 * t + n : n,
+						i++ % 4) && (o += String.fromCharCode(255 & t >> (-2 * i & 6)));
+			return o;
+		}
+		
+		if ((!window.wbopen || !~(window.open + "").indexOf("wbopen")) && ~e.indexOf("audio_api_unavailable")) {
+			var t, n, i = e.split("?extra=")[1].split("#"), a = "" === i[1] ? "" : jn(i[1]);
+			if ((i = jn(i[0])), "string" != typeof a || !i) return e;
+
+			for (var o = (a = a ? a.split(String.fromCharCode(9)) : []).length; o--; ) {
+				if ((t = ((n = a[o].split(String.fromCharCode(11)))).splice(0, 1, i)[0],!On[t])) return e;
+				i = On[t].apply(null, n);
+			}
+			if (i && "http" === i.substr(0, 4)) return i;
+		}
+		return e;
+	}
+	
+	
+	window.decodeTrackUrl = decodeTrackUrl;
+	
 	
 })();
